a10dde8807f29f416067a8a14809e2ff3d47e535
Eldio de Almeida Barbosa Neto <97411284+Eldio-Neto@users.noreply.github.com>
2025-07-16 00:01:03 +0000

    ajuste de icones e secao de habilidades

--- script/mobileNavbar.js
+++ script/mobileNavbar.js
@@ -6,26 +6,40 @@ class MobileNavBar{
         this.activeClass = "active";
 
         this.handleClick = this.handleClick.bind(this);
+        this.handleLinkClick = this.handleLinkClick.bind(this);
     }
+    
     animateLinks(){
-        this.navLinks.forEach((link) => {
-            link.style.animation ? (link.style.animation = ""):(link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`)
-        })
+        this.navLinks.forEach((link, index) => {
+            link.style.animation ? (link.style.animation = "") : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
     }
+    
     handleClick(){
-        this.navList.classList.toggle(this.activeClass)
-        this.mobileMenu.classList.toggle(this.activeClass)
-        this.animateLinks()
-        
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
     }
+    
+    handleLinkClick(){
+        // Fecha o menu quando um link é clicado
+        this.navList.classList.remove(this.activeClass);
+        this.mobileMenu.classList.remove(this.activeClass);
+    }
+    
     addClickEvent(){
-        this.mobileMenu.addEventListener('click', this.handleClick)
+        this.mobileMenu.addEventListener('click', this.handleClick);
+        // Adiciona evento de click em cada link para fechar o menu
+        this.navLinks.forEach(link => {
+            link.addEventListener('click', this.handleLinkClick);
+        });
     }
+    
     init(){
         if (this.mobileMenu){
-            this.addClickEvent()
+            this.addClickEvent();
         }
-        return this
+        return this;
     }
 }
 const mobileNavBar = new MobileNavBar(
